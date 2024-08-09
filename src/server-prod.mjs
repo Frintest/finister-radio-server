62186f1d25388c02475633a258c95d17d8a32df7
Dmitry <dimaruzaev2@gmai.com>
2024-08-09 18:47:37 +0300

    1

--- src/server-prod.mjs
+++ src/server-prod.mjs
@@ -9,23 +9,23 @@ const app = express();
 
 const directory = `/etc/letsencrypt/live/airmonitor.servermc.ru-0001`;
 const ssl = {
-	key: fs.readFileSync(`${directory}/privkey.pem`),
-	cert: fs.readFileSync(`${directory}/fullchain.pem`),
+   key: fs.readFileSync(`${directory}/privkey.pem`),
+   cert: fs.readFileSync(`${directory}/fullchain.pem`),
 };
 
 const httpsServer = https.createServer(ssl, app);
 const io = new Server(httpsServer, {
-	cors: {
-		origin: ["https://frintest.github.io"],
-	},
+   cors: {
+      origin: ["https://frintest.github.io"],
+   },
 });
 
 const onConnection = async (socket) => {
-	audioSocketHandler(socket);
+   audioSocketHandler(socket);
 };
 
 io.on("connection", onConnection);
 
 httpsServer.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+   console.log(`Server is running on port ${PORT}`);
 });
