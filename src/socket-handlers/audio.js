995a75ed7fc78062f124dc5c82ae0121c0f170bf
Dmitry <dimaruzaev2@gmai.com>
2024-08-12 22:06:11 +0300

    ref

--- src/socket-handlers/audio.js
+++ src/socket-handlers/audio.js
@@ -20,7 +20,7 @@ export const audioSocketHandler = async (socket) => {
    });
 
    const sleep = () => {
-      return new Promise((resolve) => setTimeout(resolve, 500));
+      return new Promise((resolve) => setTimeout(resolve, 1000));
    };
 
    while (isSocketConnect) {
